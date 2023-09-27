c5e3a97ff122e9d16fe96ebfdea9507ae3f2774f
Jonathan Nenba <nenbajonathan@protonmail.com>
2023-09-27 17:34:45 +0100

    feat: add header component and setup some app configs

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { SafeAreaProvider,SafeAreaView } from 'react-native-safe-area-context';
+import { Header } from './components/header';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'SF-Pro-Rounded-Bold': require('./assets/fonts/SF-Pro-Rounded-Bold.otf'),
+    'SF-Pro-Rounded-Heavy': require('./assets/fonts/SF-Pro-Rounded-Heavy.otf'),
+    'SF-Pro-Rounded-Medium': require('./assets/fonts/SF-Pro-Rounded-Medium.otf'),
+    'SF-Pro-Rounded-Regular': require('./assets/fonts/SF-Pro-Rounded-Regular.otf'),
+    'SF-Pro-Rounded-Semibold': require('./assets/fonts/SF-Pro-Rounded-Semibold.otf'),
+    'SFMono-Medium': require('./assets/fonts/SFMono-Medium.otf'),
+    'SFMono-Regular': require('./assets/fonts/SFMono-Regular.otf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if(!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider  onLayout={onLayoutRootView}>
+      <SafeAreaView style={{ marginHorizontal: 8 }} >
+        <Header />
+        <View>
+          <Text style={{ fontFamily: 'SF-Pro-Rounded-Bold' }} >Open up App.js to start working on your app!</Text>
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/header/Header.js
@@ -0,0 +1,61 @@
+import { StyleSheet, View,Text, Button, Pressable } from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+export const Header = () => {
+    return(
+        <View style={styles.container}>
+            <Pressable style={{...styles.button, ...styles.settings_button}}>
+                <Text style={{...styles.text, ...styles.settings_text_button}} >Settings</Text>
+            </Pressable>
+            <Text style={styles.logo} >
+                Scan & Save
+            </Text>
+            <Pressable style={{...styles.button, ...styles.back_up_button}}>
+                <Text style={{ ...styles.text, ...styles.back_up_text }} >Back up</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 50,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    logo: {
+        fontFamily: 'SF-Pro-Rounded-Bold',
+        fontSize: 25,
+        color: '#ed5f00'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 6,
+        paddingHorizontal: 10,
+        borderRadius: 4,
+        elevation: 3,
+        borderRadius: 20,
+    },
+    text: {
+        fontSize: 14,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        fontFamily: 'SF-Pro-Rounded-Medium',
+    },
+    back_up_button: {
+        backgroundColor: '#e9f9ee',
+    },
+    back_up_text: {
+        color: '#299764',
+    },
+    settings_button: {
+        backgroundColor: '#fff1e7',
+    },
+    settings_text_button: {
+        color: '#ed5f00',
+    }
+});
+  
